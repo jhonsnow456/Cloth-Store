d2a6ad1f9b64faf5f42fa2b92b74bae7d067a6e5
jhonsnow456 <thakuraman22july@gmail.com>
2022-12-24 00:35:16 +0530

    On branch e-commerce  Changes to be committed: 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/button/button.component.jsx 	new file:   src/components/button/button.styles.scss 	new file:   src/components/form-input/form-input.component.jsx 	new file:   src/components/form-input/form-input.styles.scss 	new file:   src/components/sign-in-form/sign-in-form.component.jsx 	new file:   src/components/sign-in-form/sign-in-form.styles.scss 	new file:   src/components/sign-up-form/sign-up-form.component.jsx 	new file:   src/components/sign-up-form/sign-up-form.styles.scss 	new file:   src/routes/authentication/authentication.jsx 	new file:   src/routes/authentication/authentication.styles.scss 	modified:   src/routes/home/home.component.jsx 	modified:   src/routes/navigation/navigation.component.jsx 	deleted:    src/routes/sign-in/sign-in.component.jsx 	modified:   src/utils/firebase/firebase.utils.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Route, Routes } from "react-router-dom";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication";
 
 const Shop = () => {
   return <h1>Shop component </h1>;
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="signIn" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
+  signInWithRedirect,
   signInWithPopup,
-  //   signInWithRedirect,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
-
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -19,36 +20,56 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({ prompt: "select_account" });
+googleProvider.setCustomParameters({ prompt: "select_account" });
 
 export const auth = getAuth(firebaseApp);
 
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, "users", userAuth.uid);
-  console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
-
-  // console.log(userSnapshot);
-  // console.log(userSnapshot.exists());
-  // console.log(userAuth);
 
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
-      await setDoc(userDocRef, { displayName, email, createdAt });
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
     } catch (error) {
-      console.log(error.message);
+      console.log("error creating the user", error.message);
     }
   }
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
