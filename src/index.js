f809eb3dda7481fc5dd2d5959e49238a6699adfd
jhonsnow456 <thakuraman22july@gmail.com>
2022-12-24 18:08:03 +0530

    On branch e-commerce  Changes to be committed: 	modified:   src/components/sign-in-form/sign-in-form.component.jsx 	modified:   src/components/sign-up-form/sign-up-form.component.jsx 	new file:   src/contexts/user.context.jsx 	modified:   src/index.js 	modified:   src/routes/navigation/navigation.component.jsx 	modified:   src/utils/firebase/firebase.utils.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+
+import "./index.scss";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,20 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-import "./navigation.styles.scss";
+
+import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { ReactComponent as Shoplogo } from "../../asset/Simple-Isometric-Store.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
+
   return (
     <>
       <div className="navigation">
@@ -14,9 +25,15 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth">
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
