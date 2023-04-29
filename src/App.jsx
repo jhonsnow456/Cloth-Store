08afe5085e192a5cca1529958a015908051efc71
jhonsnow456 <thakuraman22july@gmail.com>
2023-04-29 22:07:10 +0530

    added a test commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Authentication from "./routes/authentication/authentication";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 
+// added a test commit
 const App = () => {
   return (
     <Routes>
